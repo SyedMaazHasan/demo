96827152724e2e618470532b9bfe1010b0498996
Maaz <syed_maaz_hasan@yahoo.com>
2020-06-08 00:28:41 +0530

    my initail commit

---
+++ abcd.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+class Pagination extends Component {
+  state = {
+    prevPage: "page-link diabled"
+  };
+  render() {
+    return (
+      <nav aria-label="...">
+        <ul className="pagination">
+          <li
+            className={
+              this.props.currentPage === 1 ? "page-item disabled" : "page-item"
+            }
+          >
+            <a
+              className="page-link"
+              onClick={() => this.props.prevPageClick()}
+              href="#"
+            >
+              Previous
+            </a>
+          </li>
+
+          {this.props.PageArray.map(r => (
+            <li className="page-item" key={r}>
+              <a
+                className="page-link"
+                onClick={() => this.props.clicked(r)}
+                href="#"
+              >
+                {r}
+              </a>
+            </li>
+          ))}
+
+          <li
+            className={
+              this.props.currentPage === this.props.PageArray.length
+                ? "page-item disabled"
+                : "page-item"
+            }
+          >
+            <a
+              className="page-link"
+              onClick={this.props.nextPageClick}
+              href="#"
+            >
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Pagination;
