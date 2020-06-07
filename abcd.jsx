bebdea92fe0daa39f56fef7492114f393dc4b36e
Maaz <syed_maaz_hasan@yahoo.com>
2020-06-08 00:32:49 +0530

    my feature branch commit

--- abcd.jsx
+++ abcd.jsx
@@ -4,6 +4,7 @@ class Pagination extends Component {
     prevPage: "page-link diabled"
   };
   render() {
+      console.log("hi")
     return (
       <nav aria-label="...">
         <ul className="pagination">
